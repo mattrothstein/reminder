5ff1a614ab8506bcc6ec967a59f4c1e2614b5a2d
Matt Rothstein <it@4beauty.com>
2017-03-08 09:39:03 -0500

    troubleshoot protect from forgery

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = current_user.events
+    @events = Event.all
     respond_to do |format|
       format.html
       format.js
