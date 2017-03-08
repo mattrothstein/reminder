89bc2a47e6bb00bd72c41e5cd7f4c1c4203c3498
Matt Rothstein <it@4beauty.com>
2017-03-08 09:33:35 -0500

    troubleshoot protect from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format.json? }
   before_action :authenticate_user!
 end
